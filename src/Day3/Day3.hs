a45e2b2126f50cae0e68a7a4a29f73ed094def08
Aspen <zebington@gmail.com>
2019-12-05 23:32:31 +0000

    day 3 part 2

--- src/Day3/Day3.hs
+++ src/Day3/Day3.hs
@@ -3,32 +3,40 @@ module Day3.Day3 where
 type Point = (Integer, Integer)
 type Instruction = (Char, Integer)
 type Edge = (Point, Point)
+type P2Edge = (Edge, Integer)
 type Tranformation = (Integer -> Integer -> Integer)
 
 part1 :: [[String]] -> Integer
 part1 input = do
     let instructions = map (map getInstruction) input
-    let wires = map (getEdges (0,0)) instructions
+    let wires = map (getEdges 0 (0,0)) instructions
     let crosses = [(point1,point2) | point1 <- wires !! 0, point2 <- wires !! 1, point1 `overlaps` point2]
     minimum $ map (crossDistance . crossAt) crosses
+
+part2 :: [[String]] -> Integer
+part2 input = do
+    let instructions = map (map getInstruction) input
+    let wires = map (getEdges 0 (0,0)) instructions
+    let crosses = [(point1, point2) | point1 <- wires !! 0, point2 <- wires !! 1, point1 `overlaps` point2]
+    minimum $ map ((\(_, d) -> d) . crossAt) crosses
 
 getInstruction :: String -> Instruction
 getInstruction (instruction:distance) = (instruction, read distance)
 
-getEdges :: Point -> [Instruction] -> [Edge]
-getEdges point [] = []
-getEdges (x, y) ((direction, distance) : instructions)
-    | direction == 'R' = calcEdge (x + 1, y) (x + distance, y) 2 instructions
-    | direction == 'L' = calcEdge (x - distance, y) (x - 1, y) 1 instructions
-    | direction == 'U' = calcEdge (x, y + 1) (x, y + distance) 2 instructions
-    | direction == 'D' = calcEdge (x, y - distance) (x, y - 1) 1 instructions
+getEdges :: Integer -> Point -> [Instruction] -> [P2Edge]
+getEdges len point [] = []
+getEdges len (x, y) ((direction, distance) : instructions)
+    | direction == 'R' = calcEdge (len + distance) (x + 1, y) (x + distance, y) instructions
+    | direction == 'L' = calcEdge (len + distance) (x - 1, y) (x - distance, y) instructions
+    | direction == 'U' = calcEdge (len + distance) (x, y + 1) (x, y + distance) instructions
+    | direction == 'D' = calcEdge (len + distance) (x, y - 1) (x, y - distance) instructions
     | otherwise = []
 
-calcEdge :: Point -> Point -> Integer -> [Instruction] -> [Edge]
-calcEdge point1 point2 next instructions = (point1, point2) : (getEdges (if next == 1 then point1 else point2) instructions)
+calcEdge :: Integer -> Point -> Point -> [Instruction] -> [P2Edge]
+calcEdge len point1 point2 instructions = ((point1, point2), len) : (getEdges len point2 instructions)
 
-overlaps :: Edge -> Edge -> Bool
-overlaps edge1 edge2 = (canCross edge1 edge2) && (doCross edge1 edge2)
+overlaps :: P2Edge -> P2Edge -> Bool
+overlaps (edge1, _) (edge2, _) = (canCross edge1 edge2) && (doCross edge1 edge2)
 
 canCross :: Edge -> Edge -> Bool
 canCross edge1 edge2 = ((horizontal edge1) && (vertical edge2)) || ((vertical edge1) && (horizontal edge2))
@@ -39,17 +47,25 @@ doCross ((x11, y11), (x12, y12)) ((x21, y21), (x22, y22))
     | vertical ((x11, y11), (x12, y12))   = x11 `between` (x21, x22) && y21 `between` (y11, y12) 
     | otherwise = False
 
-crossAt :: (Edge, Edge) -> Point
-crossAt (((x11, y11), (x12, y12)), ((x21, y21), (x22, y22)))
-    | horizontal ((x11, y11), (x12, y12)) = (x21, y11)
-    | vertical ((x11, y11), (x12, y12)) = (x11, y21)
-    | otherwise = (0, 0)
+crossAt :: (P2Edge, P2Edge) -> (Point, Integer)
+crossAt ((((x11, y11), (x12, y12)), d1), (((x21, y21), (x22, y22)), d2))
+    | horizontal ((x11, y11), (x12, y12)) = ((x21, y11), (d1 + (x12 `diff` x21) + d2 + (y22 `diff` y11)))
+    | vertical ((x11, y11), (x12, y12))   = ((x11, y21), (d1 + (y12 `diff` y21) + d2 + (x22 `diff` x11)))
+    | otherwise = ((0, 0), 0)
 
-crossDistance :: Point -> Integer
-crossDistance (x, y) = (abs x) + (abs y)
+diff :: Integer -> Integer -> Integer
+diff a b
+    | a > b = b - a
+    | otherwise = a - b
+
+crossDistance :: (Point, Integer) -> Integer
+crossDistance ((x, y), _) = (abs x) + (abs y)
 
 between :: Integer -> (Integer, Integer) -> Bool
-between a (b, c) = a >= b && a <= c
+between a (b, c)
+    | b < c = a >= b && a <= c
+    | b > c = a >= c && a <= b
+    | otherwise = False
 
 horizontal :: Edge -> Bool
 horizontal ((_, y1), (_, y2)) = y1 == y2
--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,10 @@ main :: IO ()
 main = do
     fileName <- getDataFileName "day3.txt"
     input <- readFile fileName
-    putStrLn $ show $ part1 $ map csvStrings ["R8,U5,L5,D3", "U7,R6,D4,L4"]
-    putStrLn $ show $ part1 $ map csvStrings ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
-    putStrLn $ show $ part1 $ map csvStrings ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
-    putStrLn $ show $ part1 $ map csvStrings (lines input) 
+    putStrLn $ show $ part2 $ map csvStrings ["R8,U5,L5,D3", "U7,R6,D4,L4"]
+    putStrLn $ show $ part2 $ map csvStrings ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
+    putStrLn $ show $ part2 $ map csvStrings ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+    putStrLn $ show $ part2 $ map csvStrings (lines input) 
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
