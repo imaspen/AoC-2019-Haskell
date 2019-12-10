661ca6169f602b2a5ca744805b1bb75e337d5c4d
Aspen <zebington@gmail.com>
2019-12-10 01:34:38 +0000

    day 7 part 1

---
+++ src/Day7/Day7.hs
@@ -0,0 +1,118 @@
+module Day7.Day7 where
+
+import Data.List (nub)
+
+type Params = Int
+type Memory = [Int]
+type Inputs = [Int]
+type Outputs = [Int]
+type State = (Memory, Int)
+type MachineIO = (Inputs, Outputs)
+type NounVerb = (Int, Int)
+
+part1 :: Memory -> Int
+part1 memory = maximum $ map (run memory 0) combinations
+
+run :: Memory -> Int -> [Int] -> Int
+run memory i [] = i
+run memory i (input : rest) = run memory result rest
+    where (_, result : _) = runProgram (memory, 0) ([input, i], [])
+
+runProgram :: State -> MachineIO -> (Memory, Outputs)
+runProgram state io
+    | operation == 1  = add state params io
+    | operation == 2  = mul state params io
+    | operation == 3  = input state io
+    | operation == 4  = output state params io
+    | operation == 5  = jumpIfTrue state params io
+    | operation == 6  = jumpIfFalse state params io
+    | operation == 7  = lessThan state params io
+    | operation == 8  = equals state params io
+    | operation == 99 = (memory, outputs)
+    | otherwise = ([], [])
+    where (memory, address) = state
+          instruction = getAt (memory, address)
+          operation = opcode instruction
+          params = parameters instruction
+          (_, outputs) = io
+
+combinations :: [[Int]]
+combinations = filter ((5==) . length . nub) $ mapM (const [0..4]) [1..5]
+
+add :: State -> Params -> MachineIO -> (Memory, Outputs)
+add = basicTask (+)
+
+mul :: State -> Params -> MachineIO -> (Memory, Outputs)
+mul = basicTask (*)
+
+basicTask :: (Int -> Int -> Int) -> State -> Params -> MachineIO -> (Memory, Outputs)
+basicTask function (memory, address) params io = 
+    runProgram (newMemory, address + 4) io
+    where val1 = get (parameterMode 0 params) (memory, address + 1)
+          val2 = get (parameterMode 1 params) (memory, address + 2)
+          insertPos = getAt (memory, address + 3)
+          newMemory = insert memory insertPos (val1 `function` val2)
+
+input :: State -> MachineIO -> (Memory, Outputs)
+input (memory, address) ((add : rest), output) = 
+    runProgram (newMemory, address + 2) (rest, output)
+    where insertPos = getAt (memory, address + 1)
+          newMemory = insert memory insertPos add
+
+output :: State -> Params -> MachineIO -> (Memory, Outputs)
+output (memory, address) params (input, output) = 
+    runProgram (memory, address + 2) (input, newOutput)
+    where toAdd = get (parameterMode 0 params) (memory, address + 1)
+          newOutput = toAdd : output
+
+jumpIfTrue :: State -> Params -> MachineIO -> (Memory, Outputs)
+jumpIfTrue = jump (/= 0)
+
+jumpIfFalse :: State -> Params -> MachineIO -> (Memory, Outputs)
+jumpIfFalse = jump (== 0)
+
+jump :: (Int -> Bool) -> State -> Params -> MachineIO -> (Memory, Outputs)
+jump test (memory, address) params io
+    | test val  = runProgram (memory, jump) io
+    | otherwise = runProgram (memory, address + 3) io
+    where val   = get (parameterMode 0 params) (memory, address + 1)
+          jump  = get (parameterMode 1 params) (memory, address + 2)
+
+lessThan :: State -> Params -> MachineIO -> (Memory, Outputs)
+lessThan = testVal (<)
+
+equals :: State -> Params -> MachineIO -> (Memory, Outputs)
+equals = testVal (==)
+
+testVal :: (Int -> Int -> Bool) -> State -> Params -> MachineIO -> (Memory, Outputs)
+testVal test (memory, address) params io
+    | val1 `test` val2 = run 1
+    | otherwise   = run 0
+    where val1 = get (parameterMode 0 params) (memory, address + 1)
+          val2 = get (parameterMode 1 params) (memory, address + 2)
+          pos  = getAt (memory, address + 3)
+          run  = \val -> runProgram (insert memory pos val, address + 4) io
+
+insert :: Memory -> Int -> Int -> Memory
+insert memory pos val = before ++ [val] ++ after
+    where (before, _ : after) = splitAt pos memory
+
+get :: Int -> State -> Int
+get mode state
+    | mode == 0 = getValue state
+    | mode == 1 = getAt state
+
+getAt :: State -> Int
+getAt (memory, address) = memory !! address
+
+getValue :: State -> Int
+getValue (memory, address) = getAt (memory, getAt (memory, address))
+
+opcode :: Int -> Int
+opcode instruction = instruction `rem` 100
+
+parameters :: Int -> Int
+parameters instruction = instruction `quot` 100
+
+parameterMode :: Int -> Int -> Int
+parameterMode position instruction = (instruction `quot` (10 ^ position)) `rem` 10
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,16 @@
 module Main where
-import Day6.Day6
+import Day7.Day7
 import Paths_AoC2019
 
 import Data.List.Split
 
 main :: IO ()
 main = do
-    fileName <- getDataFileName "day6.txt"
+    fileName <- getDataFileName "day7.txt"
     input <- readFile fileName
-    putStrLn . show $ part2 $ lines input
+    let test1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+    let test2 = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
+    putStrLn . show $ part1 $ csvInts input
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
