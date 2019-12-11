b50eed45a71b73dcb160d9a92534f62f9cf7148f
Aspen <zebington@gmail.com>
2019-12-11 00:24:22 +0000

    day 9 part 1

--- src/Day9/Day9.hs
+++ src/Day9/Day9.hs
@@ -2,20 +2,21 @@ module Day9.Day9 where
 
 type Memory = [Int]
 type Address = Int
+type RelativeBase = Int
 type Input = [Int]
 type Output = [Int]
 type Halted = Bool
-type State = (Memory, Address, Input, Output, Halted)
+type State = (Memory, Address, RelativeBase, Input, Output, Halted)
 
 part1 :: Memory -> Output
-part1 memory input = output
-    where (outMem, _, _, output, _) = step (memory, 0, [], [], False)
+part1 memory = output
+    where (outMem, _, _, _, output, _) = step (memory ++ (repeat 0), 0, 0, [1], [], False)
 
 step :: State -> State
 step state
     | halted = state
     | otherwise = step $ execute state
-    where (_, _, _, _, halted) = state
+    where (_, _, _, _, _, halted) = state
 
 execute :: State -> State
 execute state
@@ -27,70 +28,94 @@ execute state
     | op == 6  = jump (==0) state
     | op == 7  = comp (<) state
     | op == 8  = comp (==) state
-    | op == 99 = (memory, address, input, output, True)
+    | op == 9  = offsetRel state
+    | op == 99 = (memory, address, rel, input, output, True)
     where
-        (memory, address, input, output, halted) = state 
+        (memory, address, rel, input, output, halted) = state 
         op = opcode $ getAt memory address
 
 arithmetic :: (Int -> Int -> Int) -> State -> State
-arithmetic op (memory, address, i, o, h) = (newMem, address + 4, i, o, h)
+arithmetic op state = (newMem, address + 4, r, i, o, h)
     where
-        ps = getParams memory address
-        v1 = getWithParam 0 ps memory address
-        v2 = getWithParam 1 ps memory address
-        pos = getAt memory (address + 3)
+        (memory, address, r, i, o, h) = state
+        v1 = getWithParam 0 state
+        v2 = getWithParam 1 state
+        pos = getPos 2 state
         newMem = insert memory pos (v1 `op` v2)
 
 getInput :: State -> State
-getInput (memory, address, (val : rest), o, h) = (newMem, address + 2, rest, o, h)
+getInput state = (newMem, address + 2, r, rest, o, h)
     where
-        pos = getAt memory (address + 1)
+        (memory, address, r, (val : rest), o, h) = state
+        pos = getPos 0 state
         newMem = insert memory pos val
-            
+
 putOutput :: State -> State
-putOutput (memory, address, i, output, h) = (memory, address + 2, i, newOut, h)
+putOutput state = (memory, address + 2, r, i, newOut, h)
     where 
-        ps = getParams memory address
-        val = getWithParam 0 ps memory address
+        (memory, address, r, i, output, h) = state
+        val = getWithParam 0 state
         newOut = val : output
 
 jump :: (Int -> Bool) -> State -> State
-jump test (memory, address, i, o, h) 
-    | test v1 = (memory, v2, i, o, h)
-    | otherwise = (memory, address + 3, i, o, h)
+jump test state
+    | test v1 = (memory, v2, r, i, o, h)
+    | otherwise = (memory, address + 3, r, i, o, h)
     where
-        ps = getParams memory address
-        v1 = getWithParam 0 ps memory address
-        v2 = getWithParam 1 ps memory address
+        (memory, address, r, i, o, h) = state
+        v1 = getWithParam 0 state
+        v2 = getWithParam 1 state
 
 comp :: (Int -> Int -> Bool) -> State -> State
-comp op (memory, address, i, o, h)
-    | v1 `op` v2 = (newMem 1, address + 4, i, o, h)
-    | otherwise  = (newMem 0, address + 4, i, o, h)
+comp op state
+    | v1 `op` v2 = (newMem 1, address + 4, r, i, o, h)
+    | otherwise  = (newMem 0, address + 4, r, i, o, h)
     where
-        ps = getParams memory address
-        v1 = getWithParam 0 ps memory address
-        v2 = getWithParam 1 ps memory address
-        pos = getAt memory (address + 3)
+        (memory, address, r, i, o, h) = state
+        v1 = getWithParam 0 state
+        v2 = getWithParam 1 state
+        pos = getPos 2 state
         newMem = insert memory pos
+
+offsetRel :: State -> State
+offsetRel state = (memory, address + 2, newRel, i, o, h)
+    where
+        (memory, address, rel, i, o, h) = state
+        ps = getParams memory address
+        v1 = getWithParam 0 state
+        newRel = rel + v1
 
 insert :: Memory -> Int -> Int -> Memory
 insert memory pos val = before ++ [val] ++ after
     where (before, _ : after) = splitAt pos memory
 
-getWithParam :: Int -> Int -> Memory -> Address -> Int
-getWithParam i ps m a = get (parameterMode i ps) m (a + i + 1)
+getWithParam :: Int -> State -> Int
+getWithParam i (m, a, r, _, _, _) = get r (parameterMode i ps) m (a + i + 1)
+    where ps = getParams m a
+            
+getPos :: Int -> State -> Int
+getPos offset (m, a, r, _, _, _) 
+    | mode == 0 = val
+    | mode == 2 = val + r
+    where
+        ps   = parameters $ getAt m a
+        mode = parameterMode offset ps
+        val  = getAt m (a + offset + 1)
 
-get :: Int -> Memory -> Address -> Int
-get mode
+get :: Int -> Int -> Memory -> Address -> Int
+get rel mode
     | mode == 0 = getValue
     | mode == 1 = getAt
+    | mode == 2 = getRel rel
 
 getAt :: Memory -> Address -> Int
 getAt memory = (memory !!)
 
 getValue :: Memory -> Address -> Int
 getValue memory address = getAt memory $ getAt memory address
+
+getRel :: Int -> Memory -> Address -> Int
+getRel offset memory address = getAt memory ((getAt memory address) + offset)
 
 getParams :: Memory -> Address -> Int
 getParams m a = parameters $ getAt m a
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,17 @@
 module Main where
-import Day8.Day8
+import Day9.Day9
 import Paths_AoC2019
 
 import Data.List.Split
 
 main :: IO ()
 main = do
-    fileName <- getDataFileName "day8.txt"
+    fileName <- getDataFileName "day9.txt"
     input <- readFile fileName
-    let test = "0222112222120000"
-    mapM_ putStrLn $ part2 25 6 $ allInts input
+    let test1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    let test2 = [1102,34915192,34915192,7,4,7,99,0]
+    let test3 = [104,1125899906842624,99]
+    putStrLn $ show $ part1 $ csvInts input
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
