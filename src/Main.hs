731f6cfdc032c1829093d8b81664a46fe4722bef
Aspen <hi@imaspen.dev>
2023-12-20 22:33:49 +0000

    Day 13 part 1

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,21 @@
 module Main where
 
 import Data.List.Split
-import Day12.Day12
+import Day13.Day13
 import Paths_AoC2019
 
 main :: IO ()
 main = do
-  fileName <- getDataFileName "day12.txt"
+  fileName <- getDataFileName "day13.txt"
   input <- readFile fileName
-  let points = listOfPoints input
+  let memory = csvInts input
 
-  let test = [[-8, -10, 0], [5, 5, 10], [2, -7, 3], [9, -8, -3]]
-  -- let test = [[-1, 0, 2], [2, -10, -7], [4, -8, 8], [3, 5, -1]]
+  putStrLn "Part 1:"
+  print $ part1 memory
 
-  putStrLn "Part 1 test:"
-  print $ part1 100 test
+  putStrLn "Part 2:"
 
-  putStrLn "Part 1 result:"
-  print $ part1 1000 points
-
-  putStrLn ""
-
-  putStrLn "Part 2 test:"
-  print $ part2 test
-
-  putStrLn "Part 2 result:"
-  print $ part2 points
+-- print $ part2 memory
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
