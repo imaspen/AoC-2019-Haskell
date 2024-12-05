ec0e40f2b1f5713cfd2fe8d092f21dc625d68d38
Aspen <hi@imaspen.dev>
2024-12-05 17:58:06 +0000

    day 17

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,15 @@
 module Main where
 
 import Data.List.Split
-import Day16.Day16
+import Day17.Day17
 import Paths_AoC2019
 
 main :: IO ()
 main = do
-  fileName <- getDataFileName "day16.txt"
+  fileName <- getDataFileName "day17.txt"
   input <- readFile fileName
-  fileNameTest1 <- getDataFileName "day16.test.1.txt"
-  inputTest1 <- readFile fileNameTest1
-  fileNameTest2 <- getDataFileName "day16.test.2.txt"
-  inputTest2 <- readFile fileNameTest2
-  fileNameTest3 <- getDataFileName "day16.test.3.txt"
-  inputTest3 <- readFile fileNameTest3
 
-  putStrLn "Part 2 Tests:"
-  print $ 84462026 == part2 (allInts inputTest1)
-  print $ 78725270 == part2 (allInts inputTest2)
-  print $ 53553731 == part2 (allInts inputTest3)
-
-  putStrLn ""
-  putStrLn "Part 2:"
-  print $ part2 $ allInts input
+  print $ part2 $ csvInts input
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
