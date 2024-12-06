c5d56b3be31731392e87ab45cb77bfe61712258e
Aspen <hi@imaspen.dev>
2024-12-06 23:03:53 +0000

    day 19 part 1

---
+++ src/Day19/Day19.hs
@@ -0,0 +1,143 @@
+module Day19.Day19 where
+
+type Memory = [Int]
+
+type Address = Int
+
+type RelativeBase = Int
+
+type Input = [Int]
+
+type Output = [Int]
+
+type Halted = Bool
+
+type State = (Memory, Address, RelativeBase, Input, Output, Halted)
+
+part1 :: Memory -> Int
+part1 memory = sum $ map (\y -> sum $ map (\x -> head $ runIntCode memory [y, x]) [0 .. 49]) [0 .. 49]
+
+runIntCode :: Memory -> Input -> Output
+runIntCode memory input =
+  let (_, _, _, _, output, _) = step (memory ++ repeat 0, 0, 0, input, [], False)
+   in output
+
+step :: State -> State
+step state
+  | halted = state
+  | otherwise = step $ execute state
+  where
+    (_, _, _, _, _, halted) = state
+
+execute :: State -> State
+execute state
+  | op == 1 = arithmetic (+) state
+  | op == 2 = arithmetic (*) state
+  | op == 3 = getInput state
+  | op == 4 = putOutput state
+  | op == 5 = jump (/= 0) state
+  | op == 6 = jump (== 0) state
+  | op == 7 = comp (<) state
+  | op == 8 = comp (==) state
+  | op == 9 = offsetRel state
+  | op == 99 = (memory, address, rel, input, output, True)
+  where
+    (memory, address, rel, input, output, halted) = state
+    op = opcode $ getAt memory address
+
+arithmetic :: (Int -> Int -> Int) -> State -> State
+arithmetic op state = (newMem, address + 4, r, i, o, h)
+  where
+    (memory, address, r, i, o, h) = state
+    v1 = getWithParam 0 state
+    v2 = getWithParam 1 state
+    pos = getPos 2 state
+    newMem = insert memory pos (v1 `op` v2)
+
+getInput :: State -> State
+getInput state = (newMem, address + 2, r, rest, o, h)
+  where
+    (memory, address, r, val : rest, o, h) = state
+    pos = getPos 0 state
+    newMem = insert memory pos val
+
+putOutput :: State -> State
+putOutput state = (memory, address + 2, r, i, newOut, h)
+  where
+    (memory, address, r, i, output, h) = state
+    val = getWithParam 0 state
+    newOut = val : output
+
+jump :: (Int -> Bool) -> State -> State
+jump test state
+  | test v1 = (memory, v2, r, i, o, h)
+  | otherwise = (memory, address + 3, r, i, o, h)
+  where
+    (memory, address, r, i, o, h) = state
+    v1 = getWithParam 0 state
+    v2 = getWithParam 1 state
+
+comp :: (Int -> Int -> Bool) -> State -> State
+comp op state
+  | v1 `op` v2 = (newMem 1, address + 4, r, i, o, h)
+  | otherwise = (newMem 0, address + 4, r, i, o, h)
+  where
+    (memory, address, r, i, o, h) = state
+    v1 = getWithParam 0 state
+    v2 = getWithParam 1 state
+    pos = getPos 2 state
+    newMem = insert memory pos
+
+offsetRel :: State -> State
+offsetRel state = (memory, address + 2, newRel, i, o, h)
+  where
+    (memory, address, rel, i, o, h) = state
+    ps = getParams memory address
+    v1 = getWithParam 0 state
+    newRel = rel + v1
+
+insert :: Memory -> Int -> Int -> Memory
+insert memory pos val = before ++ [val] ++ after
+  where
+    (before, _ : after) = splitAt pos memory
+
+getWithParam :: Int -> State -> Int
+getWithParam i (m, a, r, _, _, _) = get r (parameterMode i ps) m (a + i + 1)
+  where
+    ps = getParams m a
+
+getPos :: Int -> State -> Int
+getPos offset (m, a, r, _, _, _)
+  | mode == 0 = val
+  | mode == 2 = val + r
+  where
+    ps = parameters $ getAt m a
+    mode = parameterMode offset ps
+    val = getAt m (a + offset + 1)
+
+get :: Int -> Int -> Memory -> Address -> Int
+get rel mode
+  | mode == 0 = getValue
+  | mode == 1 = getAt
+  | mode == 2 = getRel rel
+
+getAt :: Memory -> Address -> Int
+getAt memory = (memory !!)
+
+getValue :: Memory -> Address -> Int
+getValue memory address = getAt memory $ getAt memory address
+
+getRel :: Int -> Memory -> Address -> Int
+getRel offset memory address = getAt memory (getAt memory address + offset)
+
+getParams :: Memory -> Address -> Int
+getParams m a = parameters $ getAt m a
+
+opcode :: Int -> Int
+opcode instruction = instruction `rem` 100
+
+parameters :: Int -> Int
+parameters instruction = instruction `quot` 100
+
+parameterMode :: Int -> Int -> Int
+parameterMode position instruction = instruction `quot` (10 ^ position) `rem` 10
--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,15 @@
 module Main where
 
 import Data.List.Split
-import Day18.Day18
+import Day19.Day19
 import Paths_AoC2019
 
 main :: IO ()
 main = do
-  fileName1 <- getDataFileName "day18.1.txt"
-  fileName2 <- getDataFileName "day18.2.txt"
-  fileName3 <- getDataFileName "day18.3.txt"
-  fileName4 <- getDataFileName "day18.4.txt"
-  input1 <- readFile fileName1
-  input2 <- readFile fileName2
-  input3 <- readFile fileName3
-  input4 <- readFile fileName4
+  fileName <- getDataFileName "day19.txt"
+  input <- readFile fileName
 
-  print $ part2 [lines input1, lines input2, lines input3, lines input4]
+  print $ part1 $ csvInts input
 
 listOfInts :: String -> [Integer]
 listOfInts content = map read (lines content)
